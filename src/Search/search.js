b40043bc5b3a165f602c2f7472e4ceb43a04ce8d
Mauro Luna <you@example.com>
2019-08-18 23:24:18 -0300

    add styles

--- src/Search/search.js
+++ src/Search/search.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { Input } from './components/search.input';
 import { Datalist } from './components/search.datalist';
+import './search.css';
 
 export function Search({submit, reset, positions}) {
   return (
-    <form onSubmit={submit}>
+    <form className="search_form" onSubmit={submit}>
       <Input name="name" />
       <Datalist identifier="position" name="position" options={positions}/>
       <Input name="age" />
